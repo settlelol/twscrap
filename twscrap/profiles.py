949f84c55ef44b2b11b668e3723d37d9e27d45df
settle <settle@null.net>
2021-01-25 21:56:02 +0000

    Update profiles.py

--- twscrap/profiles.py
+++ twscrap/profiles.py
@@ -10,6 +10,7 @@ class Profile():
         self.followers = 0
         self.likes = 0
         self.following = 0
+        self.followingTEST = []
         self.list_count = 0
         self.username = ""
         self.verified = False
@@ -28,6 +29,7 @@ class Profile():
         self.followers = data["data"]["user"]["legacy"]["followers_count"]
         self.likes = data["data"]["user"]["legacy"]["favourites_count"]
         self.following = data["data"]["user"]["legacy"]["friends_count"]
+        self.followingTEST = data["data"]["user"]["legacy"]["following"]
         self.list_count = data["data"]["user"]["legacy"]["listed_count"]
         self.username = data["data"]["user"]["legacy"]["screen_name"]
         self.verified = data["data"]["user"]["legacy"]["verified"]
